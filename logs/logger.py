20e7468661f0b42558d46fe8d3ef0cf652025454
Towett <etowett1@gmail.com>
2023-05-10 01:23:39 +0300

    logger

---
+++ logs/logger.py
@@ -0,0 +1,35 @@
+import logging
+from typing import Optional
+
+class Logger:
+    """logs info, warning and error message
+       
+       Arg:
+         - name: the name of the logger
+    """
+    def __init__(self, name: Optional[str] = __name__) -> None:
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+
+        s_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler(
+            "./logs/log.log", mode="w"
+        )
+        fmt = logging.Formatter(
+            "%(name)s:%(levelname)s - %(message)s"
+        )
+
+        s_handler.setFormatter(fmt)
+        f_handler.setFormatter(fmt)
+
+        s_handler.setLevel(logging.INFO)
+        f_handler.setLevel(logging.INFO)
+    
+    def info(self, message: str) -> None:
+        self.logger.info(message)
+    
+    def warn(self, message: str) -> None:
+        self.logger.warning(message)
+
+    def error(self, message: str) -> None:
+        self.logger.error(message, exc_info=True)
---
+++ main.py
@@ -0,0 +1,103 @@
+import json
+import pandas as pd
+from logs import Logger
+
+class ExcelToJson:
+    """Converts Excel file containing menu items into a json file"""
+    def __init__(self) -> None:
+        self.logger = Logger(self.__class__.__name__)
+        self.logger.info("*****ExcelToJson converter started*****")
+        self.xls = pd.ExcelFile("./input/Restaurant Menu Nutrients.xlsx")
+        self.sheet_names = self.xls.sheet_names
+
+        self.results = []
+
+    def __update_df_headers(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Updates the headers of the dataframe
+           
+           Arg:
+             - df: the dataframe to be updated
+           Return:
+             - A dataframe with the updated headers
+        """
+        for row in range(df.shape[0]):
+            for col in range(df.shape[1]):
+                if df.iat[row, col] == "Menu Item":        
+                    df.columns = df.iloc[row]
+
+                    return df.drop(df.index[row])
+    
+    def __update_ingredients(self, 
+                             data: dict[str, str], 
+                             menu_item: dict[str, str|list]) -> None:
+        """Updates the list of ingredients for a given menu item
+        
+           Args:
+             - data: a dictionary with the list of ingredients
+             - menu_item: a dictionary to be updated with ingredients
+        """
+        value = data["Key Ingredients"]
+
+        if value:
+            if "," in value:
+                menu_item["KeyIngredients"].extend(value.split(", "))
+            else:
+                menu_item["KeyIngredients"].append(value)
+
+    
+    def __update_nutrients(self, 
+                           data: dict[str, str],
+                           menu_item: dict[str, str|list]) -> None:
+        """Updates the list of nutrients for a given menu item
+        
+           Args:
+             - data: a dictionary with the list of nutrients
+             - menu_item: a dictionary to be updated with nutrients
+        """
+        is_nutrient = False
+
+        for key, value in data.items():
+            if value and is_nutrient:
+                menu_item["Nutrients"].append(key)
+
+            if key == "Key Ingredients":
+                is_nutrient = True
+
+    
+    def __generate_item(self,
+                        cuisine_type: str,
+                        data_dict: dict[str, str]) -> None:
+        """Generate a menu item to be inserted into json
+           
+           Args:
+             - cuisine_type: cuisine type of the menu item
+             - data_dict: dictionary to be used in generating menu item
+        """
+        menu = {"CuisinType": cuisine_type,
+                "MenuItem": data_dict["Menu Item"],
+                "KeyIngredients": [],
+                "Nutrients": [],
+                "ImageURL": ""}
+        
+        self.__update_ingredients(data_dict, menu)
+        self.__update_nutrients(data_dict, menu)
+
+        self.results.append(menu)
+    
+    def run(self) -> None:
+        """Entry point to the scraper"""
+        for sheet_name in self.sheet_names:
+            df = self.__update_df_headers(self.xls.parse(sheet_name))
+
+            if df.empty:continue
+
+            records = df.fillna(False).to_dict("records")
+
+            [self.__generate_item(sheet_name, data) for data in records]
+        
+        with open("sample1.json", "w") as file:
+            json.dump(self.results, file, indent=4)
+
+if __name__ == "__main__":
+    converter = ExcelToJson()
+    converter.run()
